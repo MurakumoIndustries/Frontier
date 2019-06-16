87e30750ae4f2d90a3950aa2a6f30cb42b529bb7
WindWT <leibao0723@gmail.com>
2019-06-16 10:52:35 +0800

    bugfix

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -30,7 +30,7 @@
             <ul class="navbar-nav my-0">
                 <li class="nav-item dropdown">
                     <a
-                        class="nav-link dropdown-toggle py-0"
+                        class="nav-link dropdown-toggle"
                         href="#"
                         role="button"
                         data-toggle="dropdown"
@@ -38,7 +38,10 @@
                         aria-expanded="false"
                     >
                         <i class="icon icon-version"></i>
-                        <div class="d-inline-block" style="vertical-align: middle;">
+                        <div
+                            class="d-inline-block"
+                            style="vertical-align: top; height: 0; margin-top: -0.325rem; padding-right: 0.25rem;"
+                        >
                             <div id="server">{{currentServer.name}}</div>
                             <div
                                 class="m-0"
@@ -150,7 +153,9 @@ export default {
                     .then(function(registration) {
                         registration &&
                             registration.unregister().then(function(r) {
-                                localStorage["MI_Frontier_Disable_Cache"] = true;
+                                localStorage[
+                                    "MI_Frontier_Disable_Cache"
+                                ] = true;
                                 location.reload();
                             });
                     });
@@ -182,6 +187,8 @@ export default {
 .icon.icon-version {
     width: 2.5rem;
     height: 2.5rem;
+    margin-top: -0.5rem;
+    margin-bottom: -0.5rem;
     opacity: 0.5;
     background-image: url(../img/version.png);
 }
